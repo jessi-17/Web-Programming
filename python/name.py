d31e2bca8aaf88ac4f2c8050d3a2796ac6f97b12
jessi-17 <jaspreet.bhagat17@gmail.com>
2023-09-30 22:01:12 +0530

    list-in-python

--- python/name.py
+++ python/name.py
@@ -1,6 +1,16 @@
-num = int(input("Enter you name:"))
+num = int(input("Enter any number:"))
 if num%2 == 0:
     print(f"{num} is even.")
 else:
     print(f"{num} is odd.")
-    
+
+
+# list 
+
+names = ["jassi","manya","kunwar"]
+print(names)
+# appending  in the list
+names.append("khyati")
+# sorting the list
+names.sort()
+print(names)
