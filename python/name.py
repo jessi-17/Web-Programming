f4b208c261d608f3e46e6fc4d9a8e7041bdabb8d
jessi-17 <jaspreet.bhagat17@gmail.com>
2023-09-30 22:13:35 +0530

    sets

--- python/name.py
+++ python/name.py
@@ -13,4 +13,23 @@ print(names)
 names.append("khyati")
 # sorting the list
 names.sort()
+#the no of elements in the list:
+print(f"The total elements in the list are: {len(names)}")
 print(names)
+
+
+#sets
+
+#an empty set
+set1 = set()
+# no element repeats more than once. 
+#adding element
+set1.add(1)
+set1.add(5)
+set1.add(2)
+set1.add(3)
+set1.add(4)
+print(set1)
+set1.remove(3)
+print(set1)
+print(f"the total number of the elements are {len(set1)}")
